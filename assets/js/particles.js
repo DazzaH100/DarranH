03fbdf7f368a60168d6cbe7a00441e2aa2758172
DazzaH100 Henry <henrydarran@yahoo.com>
2024-04-04 15:30:48 +0000

    adjustment to particles.js

---
+++ assets/js/particles.js
@@ -0,0 +1,9 @@
+particlesJS('particles-js', {
+    particles: {
+      number: { value: 80, density: { enable: true, value_area: 800 } },
+      color: { value: ['#000000', '#6A82FB', '#A4EBF3', '#ffe'] },
+      // ... more options to customize the look and feel
+    },
+    // ... other particles.js settings
+  });
+  
